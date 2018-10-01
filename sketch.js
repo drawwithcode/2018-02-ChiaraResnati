f25aa5fc5a88f8cba8e162d16e763cfe0795273e
chiaraerre <chiara.resnati@github.com>
2018-10-01 22:02:54 +0200

    Assignment

--- sketch.js
+++ sketch.js
@@ -1,11 +1,33 @@
-function preload(){
-  // put preload code here
-}
+var bg
 
 function setup() {
-  // put setup code here
+
+  createCanvas(windowWidth,windowHeight)
+  angleMode(DEGREES)
+  rectMode(CENTER)
+
 }
 
 function draw() {
-  // put drawing code here
-}
+  background(5)
+
+
+  var radius = 100+mouseX/5;
+  translate(width/2, height/2)
+  rotate (frameCount)
+  for (var i = -90; i < 90; i++) {
+    colorMode(HSB)
+    stroke(frameCount/10, 100, 100);
+    line(radius, 0,
+      radius-(radius*2*cos(i)*cos(i)),
+      -(radius*2*cos(i)*sin(i)));
+    }
+
+  noStroke()
+  colorMode(RGB)
+  fill(50, 50, 50, 60);
+  rotate (-2*frameCount)
+  translate(200, 200)
+  ellipse(0, 0, 500)
+
+  }
